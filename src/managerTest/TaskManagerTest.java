1427858ae9c8ae45871ca20a6a8e33555ce2749c
IgorPushka <igor081991@yandex.ru>
2023-07-18 11:47:58 +0300

    sprint 7, try 1.2-1

--- src/managerTest/TaskManagerTest.java
+++ src/managerTest/TaskManagerTest.java
@@ -11,7 +11,6 @@ import task.Task;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -215,60 +214,6 @@ abstract class TaskManagerTest<T extends TaskManager> {
         manager.addEpic(new Epic("epic", "description"));
         manager.addSubtask(new Subtask("subtask", "description", Status.NEW, 1));
         assertEquals(1, manager.getEpic(1).getSubtaskIds().size());
-    }
-
-    @Test
-    void getEmptyHistory() {
-        assertTrue(manager.getHistory().isEmpty());
-    }
-
-    @Test
-    void checkNoCopiesInHistory() {
-        manager.addEpic(new Epic("epic", "description"));
-        manager.getEpic(1);
-        manager.getEpic(1);
-        manager.getEpic(1);
-        assertEquals(1, manager.getHistory().size());
-    }
-
-
-    private void createEpicANdTasks() {
-        manager.addEpic(new Epic("epic", "description"));
-        manager.addTask(new Task("task", "description"));
-        manager.addTask(new Task("task", "description"));
-        manager.getEpic(1);
-        manager.getTask(2);
-        manager.getTask(3);
-    }
-
-    @Test
-    void deleteHeadFromHistory() {
-        Task task = new Task("task", "description", 2);
-        Task task1 = new Task("task", "description", 3);
-        createEpicANdTasks();
-        manager.deleteEpic(1);
-        List<Task> list= List.of(task, task1);
-        assertEquals(list, manager.getHistory());
-    }
-
-    @Test
-    void deleteTailFromHistory() {
-        Epic epic = new Epic("epic", "description", 1);
-        Task task = new Task("task", "description", 2);
-        createEpicANdTasks();
-        manager.deleteTask(3);
-        List<Task> list= List.of(epic, task);
-        assertEquals(list, manager.getHistory());
-    }
-
-    @Test
-    void deleteMiddleElementFromHistory() {
-        Epic epic = new Epic("epic", "description", 1);
-        Task task = new Task("task", "description", 3);
-        createEpicANdTasks();
-        manager.deleteTask(2);
-        List<Task> list= List.of(epic, task);
-        assertEquals(list, manager.getHistory());
     }
 
     @Test
